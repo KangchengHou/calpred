19c30df0c29fa07ed3f9a7b86ae169b8cdb93697
Kangcheng Hou <kangchenghou@gmail.com>
2023-07-25 11:41:00 -0700

    add code

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# calpred
+# CalPred (Calibrated Prediction Intervals for Polygenic Scores Across Diverse Contexts)
+
+See [companion manuscript github repository](https://github.com/KangchengHou/calpred-manuscript) for analysis scripts used in the [manuscript](https://github.com/KangchengHou/calpred-manuscript).
+
+## Example usage
---
+++ calpred.R
@@ -0,0 +1,131 @@
+#' @rdname simulate
+#'
+#' @title Simulate data using the model
+#'
+#' @description A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param mean_vars Covariates for mean effects \code{mean_covar}
+#' @param sd_vars Covariates for variance effects \code{var_covar}
+#' @param mean_coef Coefficients for mean effects \code{mean_coef}
+#' @param sd_coef Coefficients for variance effects \code{var_coef}
+#'
+#' @return Simulated data
+#'
+#' @export
+#'
+simulate <- function(mean_vars, sd_vars, mean_coef, sd_coef){
+  y_mean <- mean_vars %*% mean_coef
+  y_sd <- sqrt(exp(sd_vars %*% sd_coef))
+  y <- rnorm(n=length(y_mean), mean=y_mean, sd=y_sd)
+  return(data.frame(y=y, mean=y_mean, sd=y_sd))
+}
+
+#' @rdname train
+#'
+#' @title Train CalPred model
+#'
+#' @description A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param mean_vars Covariates for mean effects \code{mean_vars}
+#' @param sd_vars Covariates for variance effects \code{sd_vars}
+#' @param y Response variable \code{y}
+#'
+#' @return Fitted parameters
+#'
+#' @export
+#'
+train <- function(mean_vars, sd_vars, y, tol=1e-6, maxit=100) {
+  fit <- statmod::remlscore(y=y, X=mean_vars, Z=sd_vars, tol=tol, maxit=maxit)
+  mean_coef <- as.vector(fit$beta)
+  sd_coef <- as.vector(fit$gamma)
+  mean_se <- fit$se.beta
+  sd_se <- fit$se.gam
+
+  names(mean_coef) <- colnames(mean_vars)
+  names(sd_coef) <- colnames(sd_vars)
+  names(mean_se) <- colnames(mean_vars)
+  names(sd_se) <- colnames(sd_vars)
+
+  return(list(
+    mean_coef=mean_coef,
+    sd_coef=sd_coef,
+    mean_se=mean_se,
+    sd_se=sd_se
+  ))
+}
+
+#' @rdname predict
+#'
+#' @title Predict with CalPred model
+#'
+#' @description A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param mean_vars Covariates for mean effects \code{mean_vars}
+#' @param sd_vars Covariates for variance effects \code{sd_vars}
+#' @param y Response variable \code{y}
+#'
+#' @return Predicted mean and SD
+#'
+#' @export
+#'
+predict <- function(mean_vars, sd_vars, mean_coef, sd_coef) {
+  if (!all.equal(colnames(mean_vars), names(mean_coef))) {
+    stop("colnames(mean_vars) != names(mean_coef)")
+  }
+  if (!all.equal(colnames(sd_vars), names(sd_coef))) {
+    stop("colnames(sd_vars) != names(sd_coef)")
+  }
+  y_mean <- mean_vars %*% mean_coef
+  y_var <- exp(sd_vars %*% sd_coef)
+  return(data.frame(
+    mean=y_mean,
+    sd=sqrt(y_var)
+  ))
+}
+
+#' @rdname compute_stats
+#'
+#' @title Compute statistics
+#'
+#' @description A more detailed description of what the function is and how
+#' it works. It may be a paragraph that should not be separated
+#' by any spaces.
+#'
+#' @param y Response variable \code{y}
+#' @param pred Covariates for mean effects
+#' @param predstd Covariates for variance effects
+#' @param group group information
+#' @param n_bootstrap number of bootstrap
+#'
+#' @return Computed statistics
+#'
+#' @export
+#'
+compute_stats <- function(y, pred, predstd, group, n_bootstrap=0){
+  data <- data.frame(y, pred, predstd, group)
+  compute_metric <- function(data){
+    data %>%
+      group_by(group) %>%
+      summarize(
+        r2 = cor(y, pred) ** 2,
+        coverage = mean((y >= pred - 1.645 * predstd) & (y <= pred + 1.645 * predstd)),
+        length = mean(predstd)
+      )
+  }
+  stats <- compute_metric(data)
+  if (n_bootstrap <= 0){
+    return(stats)
+  }
+  bootstrap_stats <- list()
+  for (i in 1 : n_bootstrap){
+    bootstrap_stats[[i]] <- compute_metric(data[sample(nrow(data), replace = TRUE), ])
+  }
+  bootstrap_stats <- do.call(rbind, bootstrap_stats)
+  return(list(stats=stats, bootstrap_stats=bootstrap_stats))
+}
